ef0b8cf1592202d99c7fe8a6e447f46da2e44876
ItsLJcool <54383469+ItsLJcool@users.noreply.github.com>
2024-10-30 09:57:06 -0700

    ad

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 // const axios = require('axios');
 
 const app = express();
-const port = 8845; // You can change the port if needed
+const port = 10867; // You can change the port if needed
 
 // Middleware to parse JSON body
 app.use(bodyParser.json());
